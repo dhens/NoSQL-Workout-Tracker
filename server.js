5ac48b130810fd30523b7339e6400e4d2cbe60c0
Dawson Hensel <dawsonhensel@gmail.com>
2020-04-11 14:23:13 -0700

    added frontend code

--- server.js
+++ server.js
@@ -15,12 +15,40 @@ const db = mongojs(databaseUrl, collections);
 
 db.on('error', error => {
     console.log('Database Error: ', error);
-})
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + './public/index.html'));
 });
 
+//load page when user finds us
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/exercise', (req, res) => {
+    res.sendFile(path.join(__dirname + '/public/exercise.html'));
+})
+
+// have a route to submit to
+app.get('/submit', (req, res) => {
+    db.workouts.insert(req.body, (error, data) => {
+        if (err) {
+            res.send(err);
+        } else {
+            res.send(response);
+        }
+    });
+});
+
+// take the response and put it in the db
+
+// find a workout by id
+
+
+// update a workout by id
+
+// delete a workout by id
+
+// delete all workouts
+
+app.get('')
 app.listen(3000, () => {
     console.log("App running on port 3000!");
   });
